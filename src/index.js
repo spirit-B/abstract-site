8b57f116e480a5d25cd5938663ab3e772fc32d82
spirit-B <janghoon333@naver.com>
2022-02-07 13:44:28 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class Site {
     
     // Board 추가
     addBoard(board) {
-        if (this.boards.find((exist) => exist.boards === board.boards)) throw new Error();
+        if (this.boards.find((exist) => exist.name === board.name)) throw new Error();
         // for (let i = 0; i < this.boards.length; i++) {
         //     if (this.boards[i].boards === board.boards) throw new Error();
         // }
@@ -18,48 +18,63 @@ class Site {
     // boardname으로 찾기
     findBoardByName(boardname) {
         const [result] = this.boards.filter(site => {
-            if (site.boards === boardname) return site;
+            if (site.name === boardname) return site;
         });
         return result;
     }
 }
 
 class Board {
-    constructor(string) {
-        if (string === '' || string === null || !string) throw new Error();
-        else {
-            this.boards = string;
-            this.articles = [];
-            this.isInSite = false;
-        }
+    constructor(name) {
+        if (name === '' || name === null || !name) throw new Error();
+        this.name = name;
+        this.articles = [];
+        this.isInSite = false;
     }
 
     publish(article) {
         if (this.isInSite) {
+            article.id = `${this.name}-` + Math.random();
+            article.createdDate = new Date().toISOString();
+            article.isInBoard = true;
             this.articles.push(article);
         }
         else throw new Error();
     }
+
+    getAllArticles() {
+        return this.articles;
+    }
 }
 
 class Article {
-    constructor(subject, content, author) {
-        this.subject = subject;
-        this.content = content;
-        this.author = author;
+    constructor(articleContents) {
+        if (articleContents.subject === '' || articleContents.subject === null) throw new Error();
+        if (articleContents.content === '' || articleContents.content === null) throw new Error();
+        if (articleContents.author === '' || articleContents.author === null) throw new Error();
+        this.id = '';
+        this.createdDate = '';
+        this.isInBoard = false;
+        this.comments =[];
+    }
+    
+    reply(comment) {
+        if (this.isInBoard) {
+            comment.createdDate = new Date().toISOString();
+            this.comments.push(comment);
+        } else throw new Error();
+    }
+
+    getAllComments() {
+        return this.comments;
     }
 }
 
 class Comment {
-    constructor(content, author) {
-        if (content === '' || content === null) throw new Error();
-        else if (author === '' || author === null) throw new Error();
-        this.content = content;
-        this.author = author;
-    }
-
-    reply(comment) {
-        
+    constructor(commentContents) {
+        if (commentContents.content === '' || commentContents.content === null) throw new Error();
+        if (commentContents.author === '' || commentContents.author === null) throw new Error();
+        this.createdDate = '';
     }
 }
 
